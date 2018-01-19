21db2619e6b8de4e6b08c25b708f188275e2dda2
rickyfun <r.huang850@gmail.com>
2018-01-19 17:50:51 +0800

    Add funciton

--- aa.js
+++ aa.js
@@ -1,13 +1,15 @@
-var str = 'abcdeabcdeabcdec';
-var flag = true;
+function leetcode(str) {
+    var flag = true;
+    var splitWord = str.substring(0, 2);
+    var splitResultArr = str.split(splitWord);
+    var splitCompare = splitResultArr[1];
+    if (splitResultArr.length <= 2) flag = false;
+    splitResultArr.forEach(function(el, index) {
+        if (index == 0) return;
+        if (splitCompare !=  el) flag = false;
+    });
+    return flag;
+}
 
-var splitWord = str.substring(0, 2);
-var splitResultArr = str.split(splitWord);
-var splitCompare = splitResultArr[1];
-if (splitResultArr.length <= 2) flag = false;
-splitResultArr.forEach(function(el, index) {
-    if (index == 0) return;
-    if (splitCompare !=  el) flag = false;
-});
-console.log(flag)
+console.log(leetcode('abcdeabcdeabcdec'))
 
