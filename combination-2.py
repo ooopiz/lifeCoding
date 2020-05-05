b1f10dced2e69fce48456bf1d278872b6ef52dd5
ooopiz <r.huang850@gmail.com>
2020-05-06 03:07:26 +0800

    Add combination

---
+++ combination-2.py
@@ -0,0 +1,31 @@
+"""
+排序不變的各種組合
+"""
+
+def qq(arr):
+    qqlist = []
+    qqlist.append(arr)
+    if len(arr) == 1:
+        return qqlist
+
+    for index in range(len(arr)):
+        left = arr[0:index+1]
+        right = arr[index+1:]
+        #print(left)
+        #print(right)
+
+        if len(right) > 0:
+            rec = qq(right)
+            for index in range(len(rec)):
+                qqlist.append(left + [' | '] + rec[index])
+
+    return qqlist
+
+
+#arr = 'ABCD'
+#arr = ['A','B','C', 'D']
+arr = ['文字', '遊戲', '顧名思義', '就是', '只有' ,'文字']
+
+haha = qq(arr)
+for index in range(len(haha)):
+    print(haha[index])
